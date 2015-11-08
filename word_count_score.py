5a807d965d2dbddf0dbded10a8e308a3f7c26837
Bovard Tiberi <bovard.tiberi@gmail.com>
2015-11-08 15:40:37 -0800

    First output

--- word_count_score.py
+++ word_count_score.py
@@ -7,123 +7,198 @@ import stop_words
 
 
 def get_words_counts(filename):
-	"""
-	Reads in a file with reddit comment json
-	@returns a dictionary of words with scores and counts
-	example:
-	{
-		"egg": (250, 50)
-	}
-	egg was mentioned in 50 unique comments
-	the total score for all unique comments was 250
-	(so avarge would be 250/50 = 5)
-	"""
-	print "Processing {}".format(filename)
+    """
+    Reads in a file with reddit comment json
+    @returns a dictionary of words with scores and counts
+    example:
+    {
+        "egg": (250, 50)
+    }
+    egg was mentioned in 50 unique comments
+    the total score for all unique comments was 250
+    (so avarge would be 250/50 = 5)
+    """
+    print "Processing {}".format(filename)
 
-	# we'll ignore stop words (the, is, and, etc...)
-	stopwords = stop_words.get_stop_words('english')
+    # we'll ignore stop words (the, is, and, etc...)
+    stopwords = stop_words.get_stop_words('english')
 
-	words_scores = {}
-	with open(filename) as f:
-		for entry in f.readlines():
-			entry = json.loads(entry)
-			words = entry['body']
-			score = entry['score']
+    words_scores = {}
+    with open(filename) as f:
+        for entry in f.readlines():
+            entry = json.loads(entry)
+            words = entry['body']
+            score = entry['score']
 
-			# removes all non-space, non-alpha characters
-			import re
-			words = " ".join(re.findall("[a-zA-Z ']+", words))
+            # removes all non-space, non-alpha characters
+            import re
+            words = " ".join(re.findall("[a-zA-Z ']+", words))
 
-			# makes everything lowercase
-			words = words.lower()
+            # makes everything lowercase
+            words = words.lower()
 
-			# used so we only count the word once per comment
-			mentioned = [] 
-			for word in words.split(' '):
-				if word.startswith("'"):
-					word = word[1:]
-				if word.endswith("'"):
-					word = word[:-1]
+            # used so we only count the word once per comment
+            mentioned = [] 
+            for word in words.split(' '):
+                if word.startswith("'"):
+                    word = word[1:]
+                if word.endswith("'"):
+                    word = word[:-1]
 
-				word = word.strip()
-				if len(word) < 2:
-					continue
-				if word in stopwords:
-					continue
+                word = word.strip()
+                if len(word) < 2:
+                    continue
+                if word in stopwords:
+                    continue
 
-				if words_scores.get(word) is None:
-					words_scores[word] = (int(score), 1)
-				elif word not in mentioned:
-					mentioned.append(word)
-					previous = words_scores[word] 
-					words_scores[word] = (previous[0] + int(score), previous[1] + 1)
-	return words_scores
+                if words_scores.get(word) is None:
+                    words_scores[word] = (int(score), 1)
+                elif word not in mentioned:
+                    mentioned.append(word)
+                    previous = words_scores[word] 
+                    words_scores[word] = (previous[0] + int(score), previous[1] + 1)
+    return words_scores
 
 def write_words_scores(words_scores):
-	with open('output.txt', 'w') as output:
-		for word, scores in words_scores.items():
-			try:
-				output.write("{}\t{}\t{}\n".format(word, scores[0], scores[1]))
-			except Exception, e:
-				pass
+    with open('output.txt', 'w') as output:
+        for word, scores in words_scores.items():
+            try:
+                output.write("{}\t{}\t{}\n".format(word, scores[0], scores[1]))
+            except Exception, e:
+                pass
 
 def sort_and_normalize(words_counts):
-	"""
-	Normalizes and sorts the words_counts by total score
-	"""
-	sorted_words_counts = sorted(words_counts.items(), key=lambda entry: entry[1][0], reverse=True)
-	max_score = sorted_words_counts[0][1][0]
+    """
+    Normalizes and sorts the words_counts by total score
+    """
+    sorted_words_counts = sorted(words_counts.items(), key=lambda entry: entry[1][0], reverse=True)
+    max_score = sorted_words_counts[0][1][0]
 
-	normalized = []
-	for entry in sorted_words_counts:
-		word = entry[0]
-		score, count = entry[1]
-		normalized_score = round(float(score) / max_score, 3)
-		normalized.append((str(word), (normalized_score, score, count)))
+    normalized = []
+    for entry in sorted_words_counts:
+        word = entry[0]
+        score, count = entry[1]
+        normalized_score = round(float(score) / max_score, 3)
+        normalized.append((str(word), (normalized_score, score, count)))
 
-	return normalized
+    return normalized
 
 
-def get_top_bottom(words_counts, n=100):
-	"""
-	Gets the top and bottom n words by total score
-	@returns top_words, bottom_words
-	"""
-	sorted_words_counts = sorted(words_counts.items(), key=lambda entry: entry[1][0])
-	print "Bottom words"
-	for i in range(n):
-		print sorted_words_counts[i]
-	print ''
-	print "Top Words"
-	for i in range(1, n + 1):
-		print sorted_words_counts[i * -1]
+def combine(first_normalized, second_normalized):
+    """
+    Takes the top 100 words from both and compares them to the other list
+    """
+    results = get_association(first_normalized, second_normalized, 100) 
+
+    print ''
+    print 'RESULTS!'
+    for i in range(10):
+        print results[i]
+
+    return results
+
+
+def get_association(first, second, n=100):
+    to_return = []
+    print ''
+    second_words = [entry[0] for entry in second]
+    for i in range(n):
+        word = first[i][0]
+        if not word in second_words:
+            to_return.append((
+                first[i][0], 
+                first[i][1][0], 
+                0, 
+                first[i][1][1], 
+                0, 
+                first[i][1][2], 
+                0
+            ))
+        else:
+            idx = second_words.index(word)
+            to_return.append((
+                first[i][0], 
+                first[i][1][0], 
+                second[idx][1][0], 
+                first[i][1][1], 
+                second[idx][1][1], 
+                first[i][1][2], 
+                second[idx][1][2]
+            ))
+
+    print ''
+    first_words = [entry[0] for entry in first]
+    for i in range(n):
+        word = second[i][0]
+        if not word in first_words:
+            to_return.append((
+                second[i][0], 
+                0, 
+                second[i][1][0], 
+                0, 
+                second[i][1][1], 
+                0, 
+                second[i][1][2]
+            ))
+        else:
+            idx = first_words.index(word)
+            to_return.append((
+                second[i][0], 
+                first[idx][1][0], 
+                second[i][1][0], 
+                first[idx][1][1], 
+                second[i][1][1], 
+                first[idx][1][2], 
+                second[i][1][2]
+            ))
+    return to_return
+
+def write_to_csv(results, name='output.csv'):
+    # de duplicate
+    to_export = []
+    for r in results:
+        if r[0] not in [entry[0] for entry in to_export]:
+            to_export.append(r)
+    results = to_export
+    import csv
+    fieldnames = ["word", "norm score a", "norm score b", "raw score a", "raw score b", "num comments a", "num comments b"]
+    with open(name, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldnames)    
+        for row in results:
+            writer.writerow(row)
+
 
 
 def main():
-	if len(sys.argv) < 3:
-		print "You need to specify two files to process"
-		print "Example: python word_count_score.py subreddits/cats.json subredddits/dogs.json"
-		exit()
+    if len(sys.argv) < 3:
+        print "You need to specify two files to process"
+        print "Example: python word_count_score.py subreddits/cats.json subredddits/dogs.json"
+        exit()
 
-	first_file = sys.argv[1]
-	second_file = sys.argv[2]
+    first_file = sys.argv[1]
+    second_file = sys.argv[2]
 
-	first_words_counts = get_words_counts(first_file)
-	second_words_counts = get_words_counts(second_file)
+    first_words_counts = get_words_counts(first_file)
+    second_words_counts = get_words_counts(second_file)
 
-	print "Number of words found for each:"
-	print len(first_words_counts.keys()), len(second_words_counts.keys())
+    print "Number of words found for each:"
+    print len(first_words_counts.keys()), len(second_words_counts.keys())
 
-	print ''
-	print "getting top words for {}".format(first_file)
-	first_normalized = sort_and_normalize(first_words_counts)
-	for i in range(100):
-		print first_normalized[i]
-	print ''
-	print "getting top words for {}".format(second_file)
-	second_normalized = sort_and_normalize(second_words_counts)
-	for i in range(100):
-		print second_normalized[i]
+    print ''
+    print "getting top words for {}".format(first_file)
+    first_normalized = sort_and_normalize(first_words_counts)
+    for i in range(10):
+        print first_normalized[i]
+    print ''
+    print "getting top words for {}".format(second_file)
+    second_normalized = sort_and_normalize(second_words_counts)
+    for i in range(10):
+        print second_normalized[i]
+
+    results = combine(first_normalized, second_normalized)
+    write_to_csv(results)
+
 
 
 if __name__ == "__main__":
